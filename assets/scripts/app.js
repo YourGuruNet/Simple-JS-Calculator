5272ac4fe46a06c313e01bf67091368cef474b10
YourGuruNet <arvels123@inbox.lv>
2020-11-04 10:02:45 +0200

    Task Done

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,42 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  alert("The result is " + result);
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`; //show math formula description
+  outputResult(currentResult, calcDescription); // pass values to screen
 }
 
-add(5, 5);
+//parseFloat change string to integer ore number with comate
+function add() {
+  const input = parseFloat(userInput.value); // get input
+  const initialResult = currentResult; // get start value so its not changed after math function
+  currentResult = currentResult + input; // find the result
+  createAndWriteOutput("+", initialResult, input);
+}
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+function subtract() {
+  const input = parseFloat(userInput.value);
+  const initialResult = currentResult;
+  currentResult = currentResult - input;
+  createAndWriteOutput("-", initialResult, input);
+}
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+function multiply() {
+  const input = parseFloat(userInput.value);
+  const initialResult = currentResult;
+  currentResult = currentResult * input;
+  createAndWriteOutput("*", initialResult, input);
+}
 
-outputResult(currentResult, calculationDescription);
+function divide() {
+  const input = parseFloat(userInput.value);
+  const initialResult = currentResult;
+  currentResult = currentResult / input;
+  createAndWriteOutput("/", initialResult, input);
+}
+
+//When btn is clicked JS take function add
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
