e6dac03fc33482f560bcef2fef7c11431c5bb27d
YourGuruNet <arvels123@inbox.lv>
2020-11-08 10:44:08 +0200

    Updeitet with function to chek if value is valid

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,37 +1,72 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`; //show math formula description
   outputResult(currentResult, calcDescription); // pass values to screen
 }
 
-function add() {
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    currentNumber: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+//parseFloat change string to integer ore number with comate
+function calculateResult(calculationType) {
   const input = parseFloat(userInput.value); // get input
+  if (
+    (calculationType !== "ADD" &&
+      calculationType !== "Subtract" &&
+      calculationType !== "Multiply" &&
+      calculationType !== "Divide") ||
+    !input
+  ) {
+    alert("Insert valid number");
+  }
   const initialResult = currentResult; // get start value so its not changed after math function
-  currentResult = currentResult + input; // find the result
-  createAndWriteOutput("+", initialResult, input); // pass values to creat function
+  let mathOperator;
+  if (calculationType === "ADD") {
+    currentResult += input; // find the result
+    mathOperator = "+";
+  } else if (calculationType === "Subtract") {
+    currentResult -= input;
+    mathOperator = "-";
+  } else if (calculationType === "Multiply") {
+    currentResult *= input;
+    mathOperator = "*";
+  } else if (calculationType === "Divide") {
+    currentResult /= input;
+    mathOperator = "/";
+  }
+  createAndWriteOutput(mathOperator, initialResult, input); // pass values to creat function
+  writeToLog(calculationType, initialResult, input, currentResult);
+}
+
+function add() {
+  calculateResult("ADD");
 }
 
 function subtract() {
-  const input = parseFloat(userInput.value);
-  const initialResult = currentResult;
-  currentResult = currentResult - input;
-  createAndWriteOutput("-", initialResult, input);
+  calculateResult("Subtract");
 }
 
 function multiply() {
-  const input = parseFloat(userInput.value);
-  const initialResult = currentResult;
-  currentResult = currentResult * input;
-  createAndWriteOutput("*", initialResult, input);
+  calculateResult("Multiply");
 }
 
 function divide() {
-  const input = parseFloat(userInput.value);
-  const initialResult = currentResult;
-  currentResult = currentResult / input;
-  createAndWriteOutput("/", initialResult, input);
+  calculateResult("Divide");
 }
 
 //When btn is clicked JS take function add
